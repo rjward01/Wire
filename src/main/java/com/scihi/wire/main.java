c1270e2bc9dc398559b6112916ff119dda2a05ca
Unknown <rjward01@outlook.com>
2018-04-04 00:27:05 +0100

    Setup
    Setup project and .gitignore

---
+++ src/main/java/com/scihi/wire/main.java
@@ -0,0 +1,19 @@
+package com.scihi.wire;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(
+		modid = "wire", 
+		name = "Wire", 
+		version = "2018.1.01"
+		)
+public class main
+{
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
